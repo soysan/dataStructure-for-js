65559c1e934cd2d1c8c369eaa035e48271176bab
Masayuki <soy2.san@gmail.com>
2021-05-11 07:57:42 +0900

    stack

---
+++ stack.js
@@ -0,0 +1,27 @@
+import Node from "./node.js";
+
+class Stack {
+    constructor() {
+        this.head = null;
+    }
+
+    push = (data) => {
+        let temp = this.head;
+        this.head = new Node(data);
+        this.head.next = temp;
+    }
+
+    peek = () => {
+        if (this.head === null) return null;
+        return this.head.data;
+    }
+
+    pop = () => {
+        if (this.head === null) return null;
+        let temp = this.head;
+        this.head = this.head.next;
+        return temp.data;
+    }
+}
+
+export default Stack;
