4edc09c097660b5f28363e3e844d2d8393ec88a5
Masayuki <soy2.san@gmail.com>
2021-05-11 22:35:06 +0900

    import all at index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import BinarySearchTree from "./binary_tree/binaryTree.js";
+import Heap from "./binary_tree/heap.js";
+import SinglyLinkedList from "./linked_list/singlyLinkedList.js";
+import DoublyLinkedList from "./linked_list/doublyLinkedList.js";
+import Stack from "./stack_and_queue/stack.js";
+import Queue from "./stack_and_queue/queue.js";
+
+
